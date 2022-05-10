3fa55fa5a644306433e7867d549e1ce87e12466c
Gonçalo Almeida <gamertota4@gmail.com>
2022-05-10 17:10:46 +0100

    Done

--- código/api_goncalo.py
+++ código/api_goncalo.py
@@ -48,8 +48,41 @@ class InsufficientPrivilegesException(Exception):
         super(InsufficientPrivilegesException, self).__init__(message + extra_msg)
 
 
-class NoProductFound(Exception):
-    pass
+class ProductNotFound(Exception):
+    def __init__(self, endpoint, p_id):
+        self.endpoint = endpoint
+        self.p_id = p_id
+
+    def getError(self):
+        return f"No product found with id: {self.p_id}"
+
+
+class AlreadyRated(Exception):
+    def __init__(self, endpoint, p_id, p_version, o_id, rating, comment):
+        self.endpoint = endpoint
+        self.p_id = p_id
+        self.p_version = p_version
+        self.o_id = o_id
+        self.rating = rating
+        self.comment = comment
+
+    def getError(self):
+        return f"Product with id '{self.p_id}' and version '{self.p_version}' from order '{self.o_id}' already been rated: " \
+               f"Rating: '{self.rating}' \\ " \
+               f"Comment:'{self.comment}'"
+
+
+class ProductWithoutStockAvailable(Exception):
+    def __init__(self, endpoint, p_id, p_quantity, p_stock):
+        self.endpoint = endpoint
+        self.p_id = p_id
+        self.p_quantity = p_quantity
+        self.p_stock = p_stock
+
+    def getError(self):
+        return f"The seller hasn't the required quantity in stock of the product with id '{self.p_id}': " \
+               f"Quantity: '{self.p_quantity}' \\ " \
+               f"Stock: '{self.p_stock}'"
 
 
 ##########################################################
@@ -84,7 +117,7 @@ def admin_check(fail_msg):
 def db_connection():
     db = psycopg2.connect(
         user='postgres',
-        password='postgres',
+        password='postgres',  # TODO: *******
         host='127.0.0.1',
         port='5432',
         database='dbproj'
@@ -135,26 +168,25 @@ def get_product(product_id):
 
         cur.execute(statement, values)
         rows = cur.fetchall()
-        if len(rows) != 0:
-            logger.debug(rows)
+        if len(rows) == 0:
+            raise ProductNotFound('GET /product/<product_id>', product_id)
 
-            prices = [f"{i[6]} - {i[5]}" for i in rows]
-            comments = [i[4] for i in rows]
-            content = {'name': rows[0][0], 'stock': rows[0][1], 'description': rows[0][2], 'prices': prices,
-                       'rating': rows[0][3], 'comments': comments}
+        #logger.debug(rows)
 
-            # Response of the status of obtaining a product and the information obtained
-            response = {'status': StatusCodes['success'], 'results': content}
-        else:
-            raise NoProductFound
-    except NoProductFound:
-        error = f"No product found with id: {product_id}"
-        logger.error(f'GET /product/<product_id> - error: {error}')
-        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+        prices = [f"{i[6]} - {i[5]}" for i in rows]
+        comments = [i[4] for i in rows]
+        content = {'name': rows[0][0], 'stock': rows[0][1], 'description': rows[0][2], 'prices': prices,
+                   'rating': rows[0][3], 'comments': comments}
+
+        # Response of the status of obtaining a product and the information obtained
+        response = {'status': StatusCodes['success'], 'results': content}
+
+    except ProductNotFound as pnf:
+        # logger.error(f'{pnf.endpoint} - error: {pnf.getError()}')
+        response = {'status': StatusCodes['api_error'], 'errors': str(pnf.getError())}
     except (Exception, psycopg2.DatabaseError) as error:
         logger.error(f'GET /product/<product_id> - error: {error}')
         response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-
 
     finally:
         if conn is not None:
@@ -187,28 +219,50 @@ def give_rating_feedback(product_id):
             response = {'status': StatusCodes['api_error'], 'results': f'{i} is required to rate a product'}
             return flask.jsonify(response)
 
-    buyer_id = jwt.decode(flask.request.headers.get('Authorization').split(' ')[1], app.config['SECRET_KEY'],
-                          audience=app.config['SESSION_COOKIE_NAME'], algorithms=["HS256"])['user']
+    # A rating needs to be between 1 and 5
+    if not 1 <= payload['rating'] <= 5:
+        response = {'status': StatusCodes['api_error'], 'results': f'A valid rating is required to add a product'}
+        return flask.jsonify(response)
+
+    #buyer_id = jwt.decode(flask.request.headers.get('Authorization').split(' ')[1], app.config['SECRET_KEY'],
+    #                      audience=app.config['SESSION_COOKIE_NAME'], algorithms=["HS256"])['user']
+    buyer_id = "2"
 
     try:
-        # Get info about the product that will be rated (the one already bought) and insert the rating info in the "ratings" table
-        statement = 'do $$ ' \
-                    'declare ' \
-                    'order_id int; ' \
-                    'version timestamp; ' \
-                    'begin ' \
-                    '   select orders.id, product_quantities.products_version ' \
-                    '   from product_quantities, orders ' \
-                    '   where product_quantities.products_product_id = %s ' \
-                    '   and product_quantities.orders_id = orders.id ' \
-                    '   and orders.buyers_users_user_id = %s ' \
-                    '   into order_id, version; ' \
-                    '   insert into ratings values (%s, %s, order_id, %s, version, %s); ' \
-                    'end; ' \
-                    '$$; '
-        values = (product_id, buyer_id) + tuple(payload[i] for i in columns_names["ratings"][:2]) + (
-            product_id, buyer_id)
+        # Get info about the product that will be rated (the one already bought)
+        statement = 'select orders.id, product_quantities.products_version ' \
+                    'from product_quantities, orders ' \
+                    'where product_quantities.products_product_id = %s ' \
+                    'and product_quantities.orders_id = orders.id ' \
+                    'and orders.buyers_users_user_id = %s'
+        values = (product_id, buyer_id,)
+        cur.execute(statement, values)
+        rows = cur.fetchall()
+        # logger.debug(rows)
+        if len(rows) == 0:
+            raise ProductNotFound('POST /rating/<product_id>', product_id)
 
+        order_id = rows[len(rows) - 1][0]
+        version = rows[len(rows) - 1][1].strftime("%Y-%m-%d %H:%M:%S")
+        # logger.debug(order_id)
+        # logger.debug(version)
+
+        # Verify if the product have already been rated
+        statement = 'select orders_id, rating, comment ' \
+                    'from ratings ' \
+                    'where orders_id = %s ' \
+                    'and products_product_id = %s'
+        values = (order_id, product_id, )
+        cur.execute(statement, values)
+        rows = cur.fetchall()
+        # logger.debug(rows)
+
+        if len(rows) != 0:
+            raise AlreadyRated('POST /rating/<product_id>', product_id, version, order_id, rows[0][1], rows[0][2])
+
+        # Insert the rating info in the "ratings" table
+        statement = 'insert into ratings values (%s, %s, %s, %s, %s, %s)'
+        values = (payload['comment'], payload['rating'], order_id, product_id, version, buyer_id)
         cur.execute(statement, values)
 
         # Response of the rating status
@@ -217,6 +271,12 @@ def give_rating_feedback(product_id):
         # commit the transaction
         conn.commit()
 
+    except AlreadyRated as ar:
+        # logger.error(f'{ar.endpoint} - error: {ar.getError()}')
+        response = {'status': StatusCodes['api_error'], 'errors': str(ar.getError())}
+    except ProductNotFound as pnf:
+        # logger.error(f'{npf.endpoint} - error: {npf.getError()}')
+        response = {'status': StatusCodes['api_error'], 'errors': str(pnf.getError())}
     except (Exception, psycopg2.DatabaseError) as error:
         logger.error(error)
         response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
@@ -290,10 +350,7 @@ def add_product():
                                 'results': f'{j} is required to add a {product_type[:-1]}'}
                     return flask.jsonify(response)
 
-            final_statement += f'insert into {product_type} values (' + ('%s, ' * len(
-                columns_names[product_type]))[:-2] + '); ' \
-                                                     'end;' \
-                                                     '$$;'
+            final_statement += f'insert into {product_type} values (' + ('%s, ' * len(columns_names[product_type]))[:-2] + '); end; $$;'
             final_values += tuple(str(payload[i]) for i in required_input_info[product_type][:-1]) + tuple([str(product_id), version])
         else:
             response = {'status': StatusCodes['api_error'], 'results': 'valid type is required to add a product'}
@@ -351,50 +408,64 @@ def buy_products():
     if 'coupon' in payload:
         coupon_id = payload['coupon_id']
 
-    product_version_statement = 'select max(version), price from products where product_id = %s group by price '
-    campaign_statement = 'select campaigns_campaign_id from coupons where coupon_id = %s '
-    order_id_statement = 'select max(id) from orders '
-
+    product_version_statement = 'select version, price, stock from products where product_id = %s and version = (select max(version) from products where product_id = %s)'
     product_quantities_statement = 'insert into product_quantities values (%s, %s, %s, %s)'
-    order_statement = 'insert into orders (id, order_date, buyers_users_user_id) values (%s, %s, %s)'
-    order_with_campaign_statement = 'insert into orders (id, order_date, buyers_users_user_id, coupons_coupon_id, coupons_campaigns_campaign_id) values (%s, %s, %s, %s, %s)'
-
-    order_price_update_statement = 'update orders set price_total = %s where id = %s'
+    product_stock_statement = 'update products set stock = %s where product_id = %s and version = %s'
 
     order_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     total_price = 0.0  # Without coupon
-    buyers_id = jwt.decode(flask.request.headers.get('Authorization').split(' ')[1], app.config['SECRET_KEY'],
-                           audience=app.config['SESSION_COOKIE_NAME'], algorithms=["HS256"])['user']
+    #buyers_id = jwt.decode(flask.request.headers.get('Authorization').split(' ')[1], app.config['SECRET_KEY'],
+    #                       audience=app.config['SESSION_COOKIE_NAME'], algorithms=["HS256"])['user']
+    buyers_id = 2
 
     try:
+        order_id_statement = 'select max(id) from orders '
         cur.execute(order_id_statement, )
-        order_id = cur.fetchall()[0][0] + 1
+        rows = cur.fetchall()
+
+        order_id = rows[0][0] + 1 if rows[0][0] is not None else 1
         # logger.debug(f'{order_id}')
 
         if coupon_id != -1:
+            campaign_statement = 'select campaigns_campaign_id from coupons where coupon_id = %s '
             campaign_values = (coupon_id,)
+
             cur.execute(campaign_statement, campaign_values)
             campaign_id = cur.fetchall()[0][0]
+
+            order_with_campaign_statement = 'insert into orders (id, order_date, buyers_users_user_id, coupons_coupon_id, coupons_campaigns_campaign_id) values (%s, %s, %s, %s, %s)'
             order_with_campaign_values = (order_id, order_date, buyers_id, coupon_id, campaign_id)
             cur.execute(order_with_campaign_statement, order_with_campaign_values)
         else:
+            order_statement = 'insert into orders (id, order_date, buyers_users_user_id) values (%s, %s, %s)'
             order_values = (order_id, order_date, buyers_id)
             cur.execute(order_statement, order_values)
 
         for i in payload['cart']:
             # logger.debug(f'{i}')
 
-            product_version_values = (i['product_id'],)
+            product_version_values = (i['product_id'], i['product_id'],)
             cur.execute(product_version_statement, product_version_values)
             rows = cur.fetchall()
+
+            if len(rows) == 0:
+                raise ProductNotFound('POST /order', i['product_id'])
+
+            stock = rows[0][2]
+            if stock - i['quantity'] < 0:
+                raise ProductWithoutStockAvailable('POST /order', i['product_id'], i['quantity'], rows[0][2])
+
             version = rows[0][0].strftime("%Y-%m-%d %H:%M:%S")
-            # logger.debug(f'{rows[0][1]}')
             total_price += rows[0][1]
-            # logger.debug(f'{version}')
 
             product_quantities_values = (i['quantity'], order_id, i['product_id'], version)
             cur.execute(product_quantities_statement, product_quantities_values)
 
+            # Update stock of the product
+            product_stock_values = (stock - i['quantity'], i['product_id'], version)
+            cur.execute(product_stock_statement, product_stock_values)
+
+        order_price_update_statement = 'update orders set price_total = %s where id = %s'
         order_price_update_values = (total_price, order_id,)
         cur.execute(order_price_update_statement, order_price_update_values)
 
@@ -402,6 +473,12 @@ def buy_products():
         # commit the transaction
         conn.commit()
 
+    except ProductNotFound as pnf:
+        # logger.error(f'{pnf.endpoint} - error: {pnf.getError()}')
+        response = {'status': StatusCodes['api_error'], 'errors': str(pnf.getError())}
+    except ProductWithoutStockAvailable as pwsa:
+        # logger.error(f'{pwsa.endpoint} - error: {pwsa.getError()}')
+        response = {'status': StatusCodes['api_error'], 'errors': str(pwsa.getError())}
     except (Exception, psycopg2.DatabaseError) as error:
         logger.error(error)
         response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
